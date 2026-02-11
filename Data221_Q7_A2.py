5d0454e458fbc09611816f2f31273906b63d9e7a
jemimahab22-blip <jemimahab22@gmail.com>
2026-02-10 20:50:30 -0700

    Add files via upload

--- Data221_Q7_A2.py
+++ Data221_Q7_A2.py
@@ -1,26 +1,26 @@
+from http.client import responses
 
 import requests
 from bs4 import BeautifulSoup
 
-Data_science_html=requests.get("https://en.wikipedia.org/wiki/Data_science").text
-parsed_document=BeautifulSoup(Data_science_html,"html5lib")
+url="https://en.wikipedia.org/wiki/Data_science"
+headers={"User-Agent": "Mozilla/5.0"}#using mozilla inorder to access the website officially
+contents=requests.get(url,headers=headers)
+html=contents.text# this is the where i am storing the contents to be requested
+soup=BeautifulSoup(contents.text,"html.parser")
 
-title_page=parsed_document.find("title")
-if title_page:
-    print(title_page.get_text(strip=True))
-else:
-    print("Title tag not found")
-paragraph_page=parsed_document.find("div",id="mw-content-text")
-if paragraph_page:
-    parsed_output=paragraph_page.find("div",class_="mw-body-content")
 
-    if parsed_output:
-        for p in parsed_output.find_all("p",recursive=False):
-            text=p.get_text(strip=True)
-            if len(text)>=50:
-                print(text)
-                break
-    else:
-        print("Parser output div not found.")
-else:
-    print("Main content div not found")
+title_page=soup.find("title")
+print("The article's page title is: ",title_page.text.strip())
+
+#extracting the first page from the article
+
+content_division=soup.find("div",id="mw-content-text")
+first_paragraph=content_division.find_all("p")
+for paragraph in first_paragraph:
+    text=paragraph.text.strip()
+
+    if len(text)>=50: #this filters the short or empty paragraphs
+        print(text)
+        break  # this stops after the first valid paragraph
+
